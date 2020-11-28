a48e796bcff0ef3875358da725d8b5591e50bf67
MJMurphy99 <43189660+MJMurphy99@users.noreply.github.com>
2020-11-28 17:57:28 -0500

    Base Panel for the UI more a test to make sure no conflicts with pushing

---
+++ Assets/Mike/ResourceManagement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResourceManagement : MonoBehaviour
+{
+
+    public TMP_Text resourcesText;
+    public int resourcesAvaliableInt = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        resourcesText.text = "Resources: " + resourcesAvaliableInt;
+    }
+}
