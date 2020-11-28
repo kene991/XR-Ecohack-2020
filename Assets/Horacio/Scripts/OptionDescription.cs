8775227a05c93b11608e01bb9b5fb1d2ca14fe4c
Horacio Valdes <horavaldesg@gmail.com>
2020-11-28 16:42:32 -0500

    Option Selection

---
+++ Assets/Horacio/Scripts/OptionDescription.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionDescription : MonoBehaviour
+{
+    public string description;
+    public GameObject option;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+        option.SetActive(true);
+        TextHolder.textDescription = description;
+    }
+    public void optionOne()
+    {
+        
+    }
+    public void optionTwo()
+    {
+
+    }
+}
