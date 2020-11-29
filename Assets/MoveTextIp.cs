e9bbfdfa0f1f44b7b6cf2a4aaab27752fadb530a
Bing47328 <43833659+Bing47328@users.noreply.github.com>
2020-11-29 09:35:05 -0500

    FINAL PRODUCT/POLISH

---
+++ Assets/MoveTextIp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveTextIp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(0, 2, 0);
+        if (transform.localPosition.y >= 2000)
+        {
+            transform.localPosition = new Vector3(0, -785, 0);
+        }
+    }
+}
